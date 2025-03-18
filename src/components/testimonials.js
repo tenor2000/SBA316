fdf689c949d4b19e688f1aa607245af0ae29c24b
tenor2000 <tenor2000@gmail.com>
2025-03-18 15:35:47 -0400

    testimonials structured

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,11 +1,29 @@
-import { newEl } from "./utils.js";
+import { newEl, createCard } from "./utils.js";
 
-function createTestimonials() {
+function createTestimonials(testimonialObjects) {
   const testimonials = newEl("section");
   testimonials.setAttribute("id", "testimonials");
   const heading = newEl("h2");
   heading.textContent = "Testimonials";
   testimonials.appendChild(heading);
+
+  const cardContainer = newEl("div");
+  cardContainer.setAttribute("class", "card-container");
+  testimonials.appendChild(cardContainer);
+
+  testimonialObjects.forEach((testimonial) => {
+    const card = newEl("div");
+    card.style.color = "black";
+    card.style.textAlign = "center";
+    card.setAttribute("class", "card");
+    const quote = newEl("p");
+    quote.innerHTML = testimonial.quote;
+    card.appendChild(quote);
+    const author = newEl("p");
+    author.textContent = `-${testimonial.author}`;
+    card.appendChild(author);
+    cardContainer.appendChild(card);
+  });
   return testimonials;
 }
 export default createTestimonials;
--- src/data/testimonialData.js
+++ src/data/testimonialData.js
@@ -1,15 +1,16 @@
 export const testimonialData = [
   {
     quote:
-      "KIT10 AI changed my life. Instead of just posting pictures of cats on my socials during the day, KIT10 AI allows me to do it all night as well!",
+      "KIT10 AI changed my life. <br>Instead of just posting pictures of cats on my socials during the day, KIT10 AI allows me to do it all night as well!",
     author: "Donald from Atlanta, GA",
   },
   {
-    quote: "Before KIT10 AI, I only had dog. After sign up, It's cat now.",
+    quote:
+      "Before KIT10 AI, I only had dog. <br>After I sign up, it's cat now.",
     author: "Marla from Jersey City, NJ",
   },
   {
-    quote: "I once was lost, but with KIT10 AI, I am found!",
+    quote: "I once was lost, <br>but with KIT10 AI, I am found!",
     author: "John from San Francisco, CA",
   },
 ];
