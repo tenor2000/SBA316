734ea948286ce8e7d57030d922686aee14368385
tenor2000 <tenor2000@gmail.com>
2025-03-19 12:02:29 -0400

    hero adjusted to include a requirement

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,18 +1,36 @@
 import { newEl } from "./utils.js";
 
-function createHero(heroObj) {
+function createHero(heroObjArray) {
   const hero = newEl("section");
   hero.setAttribute("id", "hero");
   const img = newEl("img");
-  img.src = heroObj.src;
-  img.alt = heroObj.alt;
+  img.setAttribute("class", "magnify");
+  img.src = heroObjArray[0].src;
+  img.alt = heroObjArray[0].alt;
   hero.appendChild(img);
   // Use the parent-child-sibling relationship to navigate between elements at least once.
-  hero.firstChild.style.maxWidth = "800px";
+  hero.firstChild.style.maxWidth = "40vw";
 
   const tagline = newEl("h3");
-  tagline.textContent = heroObj.tagline;
+  tagline.textContent = heroObjArray[0].tagline;
   hero.appendChild(tagline);
+
+  let currIndex = 0;
+
+  // Modify at least one attribute of an element in response to user interaction.
+  img.addEventListener("click", () => {
+    let rand;
+
+    do {
+      rand = Math.floor(Math.random() * 10) % heroObjArray.length;
+    } while (rand == currIndex);
+
+    currIndex = rand;
+    img.src = heroObjArray[rand].src;
+    img.alt = heroObjArray[rand].alt;
+    tagline.textContent = heroObjArray[rand].tagline;
+  });
+
   return hero;
 }
 
--- src/data/heroData.js
+++ src/data/heroData.js
@@ -1,5 +1,29 @@
-export const heroData = {
-  tagline: "Cutting Edge Technology in Kitten-related SAAS AI products.",
-  src: "https://images.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  alt: "kittens",
-};
+export const heroData = [
+  {
+    tagline: "Cutting Edge Technology in Kitten-related SAAS AI Products.",
+    src: "https://images.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "kittens",
+  },
+  {
+    tagline: "Faster Speeds Than Ever Before.",
+    src: "https://images.pexels.com/photos/774731/pexels-photo-774731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "kittens",
+  },
+  {
+    tagline: "More Cats, Endless Possibilities.",
+    src: "https://images.pexels.com/photos/2558605/pexels-photo-2558605.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "kittens",
+  },
+  {
+    tagline:
+      "Cat Productivity Increased at the Touch of a Button, Then a Move of the Mouse, then Some Typing, Then Another Button Press.",
+    src: "https://images.pexels.com/photos/96938/pexels-photo-96938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "kittens",
+  },
+  {
+    tagline:
+      "Cat Productivity Increased at the Touch of a Button, Then a Move of the Mouse, then Some Typing, Then Another Button Press.",
+    src: "https://images.pexels.com/photos/257532/pexels-photo-257532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    alt: "kittens",
+  },
+];
